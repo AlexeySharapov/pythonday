54b9104c30b3544856db275b52f0c32758787f6f
alsharap <aleksei.sharapov@t-systems.com>
2020-02-16 22:03:33 +0300

    Fixes

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ def main():
 
 @app.route("/ST")
 def st_servers():
-    response_st7 = urllib.request.urlopen("http://tmv1934.devlab.de.tmo:8080/eCare/").getcode()
-    response_st2 = urllib.request.urlopen("http://tmv1934.devlab.de.tmo:8080/eCare/").getcode()
-    response_st5 = urllib.request.urlopen("http://tmv1934.devlab.de.tmo:8080/eCare/").getcode()
-    st_7 = "cd app/cli/bin && sh jboss-cli.sh --connect  --user=deploy --password=System135 --controller=remote+http://tmv1934.devlab.de.tmo:9990 --commands='ls deployment'"
+    response_st7 = urllib.request.urlopen("http://First").getcode()
+    response_st2 = urllib.request.urlopen("http://second").getcode()
+    response_st5 = urllib.request.urlopen("http://Third").getcode()
+    st_7 = "cd app/cli/bin && sh jboss-cli.sh --connect  --user=USER --password=PASSWORD --controller=remote+http://CONTROLLER --commands='ls deployment'"
     get_version = subprocess.call(st_7, shell=True)
     return (render_template('ST.html', a=response_st7, b=response_st2, c=response_st5, d=get_version, f=subprocess.call(st_7, shell=True)))
 
